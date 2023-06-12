1296c594b407a792070a650c57d410e5a4028067
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2023-06-12 20:42:45 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    delegate int Operation(int a, int b);
+
+    static void Main()
+    {
+        // Using a lambda expression to define a delegate function
+        Operation sum = (a, b) => a + b;
+
+        // Invoking the delegate
+        int result = sum(3, 4);
+        Console.WriteLine("Sum: " + result);
+
+        // Using a lambda expression in a LINQ query
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        var evenNumbers = numbers.Where(x => x % 2 == 0);
+        Console.WriteLine("Even numbers: " + string.Join(", ", evenNumbers));
+    }
+}
+
